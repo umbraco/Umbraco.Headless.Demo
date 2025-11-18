489d3bce513bdcacac2f646c33570313064cf14f
Dinh Tran <trd@umbraco.dk>
2025-11-18 21:25:48 +0700

    update frontend to support umbraco commerce 17

--- lib/constants.ts
+++ lib/constants.ts
@@ -27,6 +27,6 @@ export const VALIDATION_TAGS = {
 
 export const DEFAULT_OPTION = 'Default';
 export const HIDDEN_PRODUCT_TAG = 'hidden'; // Required
-export const UMBRACO_CONTENT_API_ENDPOINT = '/umbraco/delivery/api/v1';
+export const UMBRACO_CONTENT_API_ENDPOINT = '/umbraco/delivery/api/v2';
 export const UMBRACO_COMMERCE_API_ENDPOINT = '/umbraco/commerce/storefront/api/v1';
 export const UMBRACO_FORMS_API_ENDPOINT = '/umbraco/forms/api/v1';
