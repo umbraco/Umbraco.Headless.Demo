a5ef2faad8c87edd18d3872d2ce39a08201f7b81
Dinh Tran <trd@umbraco.dk>
2025-11-19 18:18:05 +0700

    update sitemap to match new vercel format

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,27 +3,32 @@ import { MetadataRoute } from 'next';
 
 const baseUrl = process.env.NEXT_PUBLIC_SITE_URL!;
 
-export default async function sitemap(): Promise<Promise<Promise<MetadataRoute.Sitemap>>> {
-  const routesMap = [''].map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString()
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  // Root route
+  const staticRoutes: MetadataRoute.Sitemap = [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date().toISOString()
+    }
+  ];
+
+  // Products
+  const products = await getProducts({});
+  const productRoutes = products.map((product) => ({
+    url: `${baseUrl}/product/${product.handle}`,
+    lastModified:
+      typeof product.updatedAt === 'string'
+        ? product.updatedAt
+        : new Date(product.updatedAt).toISOString()
   }));
 
-  const productsPromise = getProducts({}).then((products) =>
-    products.map((product) => ({
-      url: `${baseUrl}/product/${product.handle}`,
-      lastModified: product.updatedAt
-    }))
-  );
+  // Pages
+  const pages = await getPages();
+  const pageRoutes = pages.map((page) => ({
+    url: `${baseUrl}/${page.handle}`,
+    lastModified:
+      typeof page.updatedAt === 'string' ? page.updatedAt : new Date(page.updatedAt).toISOString()
+  }));
 
-  const pagesPromise = getPages().then((pages) =>
-    pages.map((page) => ({
-      url: `${baseUrl}/${page.handle}`,
-      lastModified: page.updatedAt
-    }))
-  );
-
-  const fetchedRoutes = (await Promise.all([productsPromise, pagesPromise])).flat();
-
-  return [...routesMap, ...fetchedRoutes];
+  return [...staticRoutes, ...productRoutes, ...pageRoutes];
 }
